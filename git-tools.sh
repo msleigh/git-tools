d4ea8b48435c6bc5bbc6583609523f2509e20cc3
msleigh <msleigh@users.noreply.github.com>
2019-03-20 18:45:50 +0000

    Use git tag instead of for-each-ref

--- git-tools.sh
+++ git-tools.sh
@@ -1,9 +1,9 @@
 function git_merged_tags {
     BRANCH="${1:-"HEAD"}"
-    git for-each-ref                      \
-      --sort='-refname'                   \
-      --sort='-authordate'                \
-      --format='%(refname),%(authordate)' \
-      --merged                            \
-      "${BRANCH}" 'refs/tags'
+    git tag                                \
+      --sort='-refname'                    \
+      --sort='-creatordate'                \
+      --format='%(refname),%(creatordate)' \
+      --merged                             \
+      "${BRANCH}"
 }
